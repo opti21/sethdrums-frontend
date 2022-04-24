85b57fd9c062294a473fdffd7fd525a68db45e70
opti21 <40129778+opti21@users.noreply.github.com>
2022-04-24 18:25:14 -0500

    Update request.ts

--- pages/api/public/request.ts
+++ pages/api/public/request.ts
@@ -8,6 +8,7 @@ import { YTApiResponse } from "../../../utils/types";
 import { parseYTDuration } from "../../../utils/utils";
 import prisma from "../../../utils/prisma";
 import Pusher from "pusher";
+import { getSession, withApiAuthRequired } from "@auth0/nextjs-auth0";
 
 if (
   !process.env.PUSHER_APP_ID ||
@@ -26,73 +27,145 @@ const pusher = new Pusher({
   useTLS: true,
 });
 
-const requestApiHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === "POST") {
-    // TODO: validation
-    const parsed = urlParser.parse(req.body.ytLink);
-    const youtubeID = parsed?.id;
+const publicRequestApiHandler = withApiAuthRequired(
+  async (req: NextApiRequest, res: NextApiResponse) => {
+    const session = getSession(req, res);
+    if (req.method === "POST") {
+      // TODO: validation
+      const parsed = urlParser.parse(req.body.ytLink);
+      const youtubeID = parsed?.id;
 
-    if (!youtubeID) {
-      return res
-        .status(400)
-        .json({ success: false, error: "not a valid youtube URL" });
-    }
+      if (!parsed) {
+        return res
+          .status(400)
+          .json({ success: false, error: "not a valid youtube URL" });
+      }
 
-    const userAlreadyRequested = await prisma.request.findFirst({
-      where: {
-        requested_by: req.body.requestedBy,
-        played: false,
-      },
-      include: {
-        Video: true,
-      },
-    });
-
-    const videoAlreadyRequested = await prisma.request.findFirst({
-      where: {
-        Video: {
-          youtube_id: parsed?.id,
+      const userAlreadyRequested = await prisma.request.findFirst({
+        where: {
+          requested_by: req.body.requestedBy,
+          played: false,
         },
-        played: false,
-      },
-      include: {
-        Video: true,
-      },
-    });
+        include: {
+          Video: true,
+        },
+      });
 
-    if (userAlreadyRequested) {
-      return res.status(401).send("User Already requested");
-    }
+      const videoAlreadyRequested = await prisma.request.findFirst({
+        where: {
+          Video: {
+            youtube_id: parsed?.id,
+          },
+          played: false,
+        },
+        include: {
+          Video: true,
+        },
+      });
 
-    if (videoAlreadyRequested) {
-      return res.status(402).send("Video Already requested");
-    }
+      if (userAlreadyRequested) {
+        return res.status(401).send("User Already requested");
+      }
 
-    // Check if video is in database
-    const videoInDB = await prisma.video.findFirst({
-      where: {
-        youtube_id: parsed.id,
-      },
-    });
+      if (videoAlreadyRequested) {
+        return res.status(402).send("Video Already requested");
+      }
 
-    if (!videoInDB) {
-      // Video doesn't exist on database
-      // Make new video then request
-      const createdVideo = await createVideo(youtubeID);
+      // Check if video is in database
+      const videoInDB = await prisma.video.findFirst({
+        where: {
+          youtube_id: parsed.id,
+        },
+      });
 
-      if (createdVideo) {
-        const createdRequest = await createRequest(
-          createdVideo.id,
-          req.body.requestedBy
-        );
+      if (!videoInDB) {
+        // Video doesn't exist on database
+        // Make new video then request
+        const createdVideo = await createVideo(youtubeID);
 
-        const addedToQueue = await addToQueue(createdRequest?.id.toString());
+        if (createdVideo) {
+          const createdRequest = await createRequest(
+            createdVideo.id,
+            session.user.preferred_username
+          );
 
-        if (!addedToQueue) {
-          return res
-            .status(500)
-            .json({ success: false, error: "Error adding to queue" });
+          const addedToQueue = await addToQueue(createdRequest?.id.toString());
+
+          if (!addedToQueue) {
+            return res
+              .status(500)
+              .json({ success: false, error: "Error adding to queue" });
+          }
+
+          pusher.trigger(
+            process.env.NEXT_PUBLIC_PUSHER_CHANNEL,
+            "update-queue",
+            {}
+          );
+          return res.status(200).json({ success: true });
         }
+        return;
+      }
+
+      if (videoInDB.banned) {
+        return res
+          .status(422)
+          .json({ success: false, message: "Video is banned" });
+      }
+
+      // If video is already in DB just create a request
+      const createdRequest = await createRequest(
+        videoInDB.id,
+        req.body.requestedBy
+      );
+
+      if (!createRequest) {
+        return res
+          .status(500)
+          .json({ success: false, error: "Error creating request" });
+      }
+
+      const addedToQueue = await addToQueue(createdRequest?.id.toString());
+
+      if (!addedToQueue) {
+        return res
+          .status(500)
+          .json({ success: false, error: "Error adding to queue" });
+      }
+
+      pusher.trigger(
+        process.env.NEXT_PUBLIC_PUSHER_CHANNEL,
+        "update-queue",
+        {}
+      );
+      return res.status(200).json({ success: true, message: "Request added" });
+    } else if (req.method === "DELETE") {
+      try {
+        const request = await prisma.request.findFirst({
+          where: {
+            id: req.body.requestID,
+          },
+        });
+
+        if (!request) {
+          return res
+            .status(422)
+            .json({ success: false, message: "Request does not exsist" });
+        }
+
+        if (request.requested_by != session.preferred_username) {
+          return res.status(406).json({
+            success: false,
+            message: "Hey you shouldn't be doing that, it's not yours",
+          });
+        }
+        await prisma.request.delete({
+          where: {
+            id: req.body.requestID,
+          },
+        });
+
+        await removeFromOrder(req.body.requestID.toString());
 
         pusher.trigger(
           process.env.NEXT_PUBLIC_PUSHER_CHANNEL,
@@ -100,66 +173,19 @@ const requestApiHandler = async (req: NextApiRequest, res: NextApiResponse) => {
           {}
         );
         return res.status(200).json({ success: true });
+      } catch (err) {
+        console.error(err);
+        return res
+          .status(500)
+          .json({ success: false, message: "error deleting request" });
       }
-      return;
+    } else {
+      return res.status(405).send(`${req.method} is not a valid method`);
     }
-
-    if (videoInDB.banned) {
-      return res
-        .status(422)
-        .json({ success: false, message: "Video is banned" });
-    }
-
-    // If video is already in DB just create a request
-    const createdRequest = await createRequest(
-      videoInDB.id,
-      req.body.requestedBy
-    );
-
-    if (!createRequest) {
-      return res
-        .status(500)
-        .json({ success: false, error: "Error creating request" });
-    }
-
-    const addedToQueue = await addToQueue(createdRequest?.id.toString());
-
-    if (!addedToQueue) {
-      return res
-        .status(500)
-        .json({ success: false, error: "Error adding to queue" });
-    }
-
-    pusher.trigger(process.env.NEXT_PUBLIC_PUSHER_CHANNEL, "update-queue", {});
-    return res.status(200).json({ success: true, message: "Request added" });
-  } else if (req.method === "DELETE") {
-    try {
-      const removedRequest = await prisma.request.delete({
-        where: {
-          id: req.body.requestID,
-        },
-      });
-
-      await removeFromOrder(req.body.requestID.toString());
-
-      pusher.trigger(
-        process.env.NEXT_PUBLIC_PUSHER_CHANNEL,
-        "update-queue",
-        {}
-      );
-      return res.status(200).json({ success: true });
-    } catch (err) {
-      console.error(err);
-      return res
-        .status(500)
-        .json({ success: false, message: "error deleting request" });
-    }
-  } else {
-    return res.status(405).send(`${req.method} is not a valid method`);
   }
-};
+);
 
-export default requestApiHandler;
+export default publicRequestApiHandler;
 
 async function createVideo(videoID: string): Promise<Video | undefined> {
   try {
