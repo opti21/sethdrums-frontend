b4181fdd8a978450e3dfe4e881d03531178928a6
opti21 <40129778+opti21@users.noreply.github.com>
2022-04-29 03:03:57 -0500

    style: Notis will now not pause

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -62,7 +62,17 @@ function App({ Component, pageProps }: AppProps) {
             fetcher,
           }}
         >
-          <ToastContainer />
+          <ToastContainer
+            position="top-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss={false}
+            draggable
+            pauseOnHover
+          />
           <GrowthBookProvider growthbook={growthbook}>
             <ChakraProvider>
               <ColorModeScript
