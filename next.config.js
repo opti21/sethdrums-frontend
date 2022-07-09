e13f14aed71fde0874e806b3f51f65472bc7aea2
opti21 <40129778+opti21@users.noreply.github.com>
2022-07-08 20:04:03 -0500

    fix: merch redirect

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
-const { withAxiom } = require("next-axiom");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: "/merch",
-        destination: "https://sethdrums-shop.fourthwall.com",
-        permanent: true,
-      },
-    ];
-  },
 };
 
-module.exports = withAxiom({ nextConfig });
+module.exports = { nextConfig };
