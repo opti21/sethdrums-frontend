9b59e6463efd20e76afdd24f1f5d05596b3abfd6
Brandon (opti) <40129778+opti21@users.noreply.github.com>
2023-02-04 16:56:38 -0600

    fix stupid things

--- pages/terms-conditions.tsx
+++ pages/terms-conditions.tsx
@@ -14,15 +14,15 @@ export default function Merch() {
 
       <p>Welcome to SethDrums Song Panel!</p>
 
-      <p>These terms and conditions outline the rules and regulations for the use of SethDrum LLC's Website, located at https://sethdrums.com.</p>
+      <p>These terms and conditions outline the rules and regulations for the use of SethDrum LLC{`'`}s Website, located at https://sethdrums.com.</p>
 
       <p>By accessing this website we assume you accept these terms and conditions. Do not continue to use Seth Drums Song Panel if you do not agree to take all of the terms and conditions stated on this page.</p>
 
-      <p>The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: "Client", "You" and "Your" refers to you, the person log on this website and compliant to the Company’s terms and conditions. "The Company", "Ourselves", "We", "Our" and "Us", refers to our Company. "Party", "Parties", or "Us", refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client’s needs in respect of provision of the Company’s stated services, in accordance with and subject to, prevailing law of Netherlands. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same.</p>
+      <p>The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: {`"`}Client{`"`}, {`"`}You{`"`} and {`"`}Your{`"`} refers to you, the person log on this website and compliant to the Company’s terms and conditions. {`"`}The Company{`"`}, {`"`}Ourselves{`"`}, {`"`}We{`"`}, {`"`}Our{`"`} and {`"`}Us{`"`}, refers to our Company. {`"`}Party{`"`}, {`"`}Parties{`"`}, or {`"`}Us{`"`}, refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client’s needs in respect of provision of the Company’s stated services, in accordance with and subject to, prevailing law of Netherlands. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same.</p>
 
       <Heading size='lg'><strong>Cookies</strong></Heading>
 
-      <p>We employ the use of cookies. By accessing Seth Drums Song Panel, you agreed to use cookies in agreement with the SethDrum LLC's Privacy Policy. </p>
+      <p>We employ the use of cookies. By accessing Seth Drums Song Panel, you agreed to use cookies in agreement with the SethDrum LLC{`'`}s Privacy Policy. </p>
 
       <p>Most interactive websites use cookies to let us retrieve the user’s details for each visit. Cookies are used by our website to enable the functionality of certain areas to make it easier for people visiting our website. Some of our affiliate/advertising partners may also use cookies.</p>
 
@@ -95,7 +95,7 @@ export default function Merch() {
         <ListItem>By use of any other description of our Website being linked to that makes sense within the context and format of content on the linking party’s site.</ListItem>
       </UnorderedList>
 
-      <p>No use of SethDrum LLC's logo or other artwork will be allowed for linking absent a trademark license agreement.</p>
+      <p>No use of SethDrum LLC{`'`}s logo or other artwork will be allowed for linking absent a trademark license agreement.</p>
 
       <Heading size='lg'><strong>iFrames</strong></Heading>
 
