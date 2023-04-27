64f9619c6cf31d7650a7752cadf3c0fab7f56226
Brandon (opti) <40129778+opti21@users.noreply.github.com>
2023-04-27 09:34:50 -0500

    fix: assert

--- pages/api/kick/song-request.ts
+++ pages/api/kick/song-request.ts
@@ -14,10 +14,10 @@ import Pusher from "pusher";
 dayjs.extend(utc);
 
 const pusher = new Pusher({
-  appId: process.env.PUSHER_APP_ID,
-  key: process.env.PUSHER_KEY,
-  secret: process.env.PUSHER_SECRET,
-  cluster: process.env.PUSHER_CLUSTER,
+  appId: process.env.PUSHER_APP_ID!,
+  key: process.env.PUSHER_KEY!,
+  secret: process.env.PUSHER_SECRET!,
+  cluster: process.env.PUSHER_CLUSTER!,
   useTLS: true,
 });
 
